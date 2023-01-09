23089b871f5fcf16db1d888591315ded765a83eb
Andrew Kiverin <kiverin03@yandex.ru>
2023-01-09 16:06:04 +0300

    add details in list levels, add filter for locations levels, add styles

--- src/components/LevelsHome.vue
+++ src/components/LevelsHome.vue
@@ -5,8 +5,8 @@
             <h2 class="levels-list__title">Узнайте подробнее о&nbsp;локациях в&nbsp;игре</h2>
             <form class="levels-list__form">
                 <div class="levels-list__field">
-                    <label for="inputLevelsTitle">Введите название или описание</label>
-                    <input type="text" id="inputLevelsTitle" v-model="search" class="levels-list__input">
+                    <label for="inputLevelsTitle">Поиск уровня</label>
+                    <input type="text" id="inputLevelsTitle" v-model="search" class="levels-list__input" placeholder="Название или описание уровня">
                 </div>
                 <div class="levels-list__field">
                     <label for="dropdownLevelsSort">Сортировка списка</label>
@@ -17,22 +17,40 @@
                         <option value="coins">По очкам</option>
                     </select>
                 </div>
+                <div class="levels-list__field levels-list__field_row">
+                    <p class="levels-list__filter">Фильтрация по локации</p>
+                    <div class="levels-list__checkboxs">
+                        <label for="SkylandLocationCheckbox" class="levels-list__label">Skyland
+                            <input type="checkbox" class="levels-list__checkbox" id="SkylandLocationCheckbox"
+                                value="Skyland" v-model="locations"></label>
+                        <label for="MountainVillageLocationCheckbox" class="levels-list__label">Mountain village
+                            <input type="checkbox" class="levels-list__checkbox" id="MountainVillageLocationCheckbox"
+                                value="Mountain village" v-model="locations"></label>
+                        <label for="DesertBlissLocationCheckbox" class="levels-list__label">The Desert of Bliss
+                            <input type="checkbox" class="levels-list__checkbox" id="DesertBlissLocationCheckbox"
+                                value="The Desert of Bliss" v-model="locations"></label>
+                        <label for="TowerRockLocationCheckbox" class="levels-list__label">The tower on the rock
+                            <input type="checkbox" class="levels-list__checkbox" id="TowerRockLocationCheckbox"
+                                value="The tower on the rock" v-model="locations"></label>
+                        <label for="SorcererrRockLocationCheckbox" class="levels-list__label">Sorcerer's Rock
+                            <input type="checkbox" class="levels-list__checkbox" id="SorcererRockLocationCheckbox"
+                                value="Sorcerer's Rock" v-model="locations"></label>
+                    </div>
+                </div>
             </form>
             <ul class="levels-list__list">
-                <li 
-                class="levels-list__item levelsItem" 
-                v-for="(item, index) in listLevels()" 
-                :key="index" 
-                v-bind:class="{
-                    levelsItem_easy: item.difficulty == 'easy',
-                    levelsItem_norm: item.difficulty == 'normal',
-                    levelsItem_hard: item.difficulty == 'hard',
-                }"
-            >
+                <li class="levels-list__item levelsItem" v-for="(item, index) in listLevels()" :key="index"
+                    v-bind:class="{
+                        levelsItem_easy: item.difficulty == 'easy',
+                        levelsItem_norm: item.difficulty == 'normal',
+                        levelsItem_hard: item.difficulty == 'hard',
+                    }">
                     <h3 class="levels-list__name">{{ item.name }}</h3>
                     <p class="levels-list__comment">{{ item.description }}</p>
-                    <p class="levels-list__comment">{{ item.difficulty }}</p>
-                    <router-link to="/">Подробнее</router-link>
+                    <div class="levels-list__details">
+                        <p class="levels-list__coins">{{ item.cost }}</p>
+                        <router-link class="levels-list__button" :to="'/levels/'+item.id">Подробнее</router-link>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -47,13 +65,14 @@ export default {
         return {
             search: '',
             sort: 'number',
+            locations: ['Skyland','Mountain village','The Desert of Bliss','The tower on the rock',"Sorcerer's Rock"],
         }
     },
     methods: {
         listLevels() {
-            if (this.sort == 'difficulty') {return this.sortByDifficulty(this.searchLevels())}
-            if (this.sort == 'coins') {return this.sortByCoins(this.searchLevels())}
-            if (this.sort == 'words') {return this.sortByName(this.searchLevels())}
+            if (this.sort == 'difficulty') { return this.sortByDifficulty(this.searchLevels()) }
+            if (this.sort == 'coins') { return this.sortByCoins(this.searchLevels()) }
+            if (this.sort == 'words') { return this.sortByName(this.searchLevels()) }
             return this.searchLevels()
         },
         sortByCoins(list) {
@@ -61,19 +80,24 @@ export default {
                 return a.coins - b.coins;
             });
         },
-        sortByName(list){
+        filterByLocations(list){
+            return list.filter(level => {
+                return this.locations.some(phrase => level.name.includes(phrase))
+      })
+        },
+        sortByName(list) {
             return list.sort((a, b) => {
                 if (a.name < b.name) {
-                return -1;
+                    return -1;
                 }
                 if (a.name > b.name) {
-                return 1;
+                    return 1;
                 }
                 return 0;
             });
         },
         searchLevels() {
-            let list = this.levels;
+            let list = this.filterByLocations(this.levels);
             let search = this.search;
             return list.filter(function (item) {
                 return (item.name.toLowerCase().includes(search.toLowerCase()) || item.description.toLowerCase().includes(search.toLowerCase()));
